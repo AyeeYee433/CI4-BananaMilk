7c33f637e2a82daead674b79bcd8797385571199
AyeeYee344 <aielwrenramos@gmail.com>
2025-11-13 05:57:48 +0800

    feat(about): update

--- backend/app/Views/user/about.php
+++ backend/app/Views/user/about.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 
 <head style="background-color: Blue">
@@ -230,84 +230,84 @@
         </div>
     </div> -->
 
-    <?= view('Components/cards/header') ?>
-    <!-- moodboard -->
-    <div class="moodboard">
-        <h1>Moodboard</h1>
-        <div class="moodbI">
-            <div class="moodcolor">
-                <div class="colors" style="background-color: #FCE7C8; color: #1d3557;" title="Banana Cream">#FCE7C8</div>
-                <div class="colors" style="background-color: #ede6ddff; color: #1d3557;" title="Oat Milk">#Ede6ddff</div>
-                <div class="colors" style="background-color: #C2A68C; color: white;" title="Dark Coffee">#C2A68C</div>
-                <div class="colors" style="background-color: #D97D55; color: white;" title="Blue Accent">#D97D55</div>
-            </div>
+<?= view('Components/cards/header') ?>
+<!-- moodboard -->
+<div class="moodboard">
+    <h1>Moodboard</h1>
+    <div class="moodbI">
+        <div class="moodcolor">
+            <div class="colors" style="background-color: #FCE7C8; color: #1d3557;" title="Banana Cream">#FCE7C8</div>
+            <div class="colors" style="background-color: #ede6ddff; color: #1d3557;" title="Oat Milk">#Ede6ddff</div>
+            <div class="colors" style="background-color: #C2A68C; color: white;" title="Dark Coffee">#C2A68C</div>
+            <div class="colors" style="background-color: #D97D55; color: white;" title="Blue Accent">#D97D55</div>
+        </div>
 
-            <!-- buttons -->
-            <div class="mt-3 container">
-                <button type="button" class="btn btn-primary">Primary</button>
-                <button type="button" class="btn btn-secondary">Secondary</button>
-                <button type="button" class="btn-outline-primary btn">Border</button>
-                <button type="button" class="btn btn-primary" disabled>Disabled</button>
-            </div>
+        <!-- buttons -->
+        <div class="mt-3 container">
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn-outline-primary btn">Border</button>
+            <button type="button" class="btn btn-primary" disabled>Disabled</button>
+        </div>
 
-            <div class="moodi">
-                <h2>Soft Cafe Vibe</h2>
-                <img src="https://anearthlyparadise.com/wp-content/uploads/2024/08/cardinal-interior.jpg" alt="Soft">
-                <p>Focus on warm natural visuals</p>
-            </div>
+        <div class="moodi">
+            <h2>Soft Cafe Vibe</h2>
+            <img src="https://anearthlyparadise.com/wp-content/uploads/2024/08/cardinal-interior.jpg" alt="Soft">
+            <p>Focus on warm natural visuals</p>
+        </div>
 
-            <div class="moodi">
-                <h2>Product Focus</h2>
-                <img src="https://fooddaydreaming.com/wp-content/uploads/2021/07/Banana2-1.jpg" alt="Banana Treat">
-                <p>Shows different food created with Banana</p>
-            </div>
+        <div class="moodi">
+            <h2>Product Focus</h2>
+            <img src="https://fooddaydreaming.com/wp-content/uploads/2021/07/Banana2-1.jpg" alt="Banana Treat">
+            <p>Shows different food created with Banana</p>
+        </div>
 
-            <div class="moodi">
-                <h2>Texture, Materials, and Environment</h2>
-                <img src="https://images.adsttc.com/media/images/67e3/e20b/8da1/ef01/8749/5184/newsletter/thila-bistro-and-cafe-3fconcept_20.jpg?1742987804" alt="Texture">
-                <p>Customer friendly cafe where natural materials like woods to create a soft and warm visual environment where cutomers, young and old can come and relax.</p>
-            </div>
+        <div class="moodi">
+            <h2>Texture, Materials, and Environment</h2>
+            <img src="https://images.adsttc.com/media/images/67e3/e20b/8da1/ef01/8749/5184/newsletter/thila-bistro-and-cafe-3fconcept_20.jpg?1742987804" alt="Texture">
+            <p>Customer friendly cafe where natural materials like woods to create a soft and warm visual environment where cutomers, young and old can come and relax.</p>
+        </div>
 
 
-            <div class="moodi">
-                <h2>Visual Element</h2>
-                <img src="https://mindfuldesignconsulting.com/wp-content/uploads/2022/04/Boho-Style-Restaurant-Design-with-Plants.jpg" alt="visual">
-                <p>Shows banana tree murals and plants for a open like scene</p>
-            </div>
+        <div class="moodi">
+            <h2>Visual Element</h2>
+            <img src="https://mindfuldesignconsulting.com/wp-content/uploads/2022/04/Boho-Style-Restaurant-Design-with-Plants.jpg" alt="visual">
+            <p>Shows banana tree murals and plants for a open like scene</p>
+        </div>
+    </div>
+</div>
+
+
+<!-- roadmap -->
+<div class="roadd">
+    <h1>Roadmap</h1>
+    <div class="roads">
+        <div class="step">
+            <h2>Step 1: Brainstorming</h2>
+            <p>Brainstorming ideas</p>
+        </div>
+        <div class="step">
+            <h2>Step 2: Creation</h2>
+            <p>Creation of brand, menu, food, drinks, and dessert</p>
+        </div>
+        <div class="step">
+            <h2>Step 3: Store</h2>
+            <p>Store creation</p>
+        </div>
+        <div class="step">
+            <h2>Step 4: Opening</h2>
+            <p>Shop opening</p>
         </div>
     </div>
 
+</div>
 
-    <!-- roadmap -->
-    <div class="roadd">
-        <h1>Roadmap</h1>
-        <div class="roads">
-            <div class="step">
-                <h2>Step 1: Brainstorming</h2>
-                <p>Brainstorming ideas</p>
-            </div>
-            <div class="step">
-                <h2>Step 2: Creation</h2>
-                <p>Creation of brand, menu, food, drinks, and dessert</p>
-            </div>
-            <div class="step">
-                <h2>Step 3: Store</h2>
-                <p>Store creation</p>
-            </div>
-            <div class="step">
-                <h2>Step 4: Opening</h2>
-                <p>Shop opening</p>
-            </div>
-        </div>
-
-    </div>
-
-    <!-- footer -->
+<!-- footer -->
 
 
-    <?= view('Components/cards/footer') ?>
+<?= view('Components/cards/footer') ?>
 
 
-    <body>
+<body>
 
-</html>
+    </html> -->
