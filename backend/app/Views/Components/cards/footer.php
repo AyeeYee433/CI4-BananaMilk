c1e352de6f948b1e77a8afac589549b40f4e9b57
AyeeYee433 <aielwrenramos@gmail.com>
2025-11-25 20:43:13 +0800

    feat(footer):update

--- backend/app/Views/Components/cards/footer.php
+++ backend/app/Views/Components/cards/footer.php
@@ -1,11 +1,18 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 <style>
     footer {
-        text-align: center;
+        text-align: left;
         justify-content: center;
-        margin-top: 10px;
-        font-size: 8px;
-        padding: 1rem;
+        /* margin-top: 10px; */
+        font-size: 18px;
+        padding: 40px 8%;
         background-color: #ede6ddff;
+        /* font-size: 1em; */
+        /* border-radius: 10; */
+
     }
 
     /* .footi {
@@ -15,27 +22,180 @@
         /* translate: -40% 100%; */
     }
 
-    */ #cop {
-        display: block;
+    .top {
+        display: flex;
+        padding-bottom: 0;
+        border-bottom: 1px solid#ccc;
+        width: 100%;
+        justify-content: space-between;
+        flex-wrap: nowrap;
+        /* align-self: flex-start; */
+        /* font-size: 1em; */
+        width: 100%;
+        overflow: auto;
     }
 
+    .top div {
+        width: 33%;
+        float: left;
+    }
+
+    /* #cop {
+        display: block;
+    } */
+
+    /* .top {
+        display: flex;
+        flex-wrap: wrap;
+    } */
+
+    /* .add {
+        /* transform: translate(-289%); */
+    /* border-right: 20px;
+    border-left: 20px;
+    padding-left: 20%;
+    padding-right: 20px;
+    } */
+
+
+    /* .add  */
+
+    /* .footi {
+        /* display: flex;
+        flex-wrap: wrap; */
+    /* transform: translate(-120%); */
+    /* border-right: 5px;
+        border-left: 5px;
+        padding-left: 30px; 
+    } */
+
+    .fooboard {
+        margin-left: auto;
+        margin-right: auto;
+        width: 79%;
+        padding: 40px 0;
+    }
 
     /* .bbtn {
         opacity: 0.5;
+    } */
+    .return {
+        /* align-self: left; */
+        /* margin-left: 5px; */
+        text-align: right;
+        margin-top: 30px;
+    }
+
+    .service ul,
+    .top>div>div ul {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 5px;
+    }
+
+    /* .fortie {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-top: 1px solid #ccc;
+        padding-top: 16px;
+    } */
+
+
+    /* .top>div {
+        text-align: left;
+    } */
+
+    /* li {
+        list-style: none;
+    } */
+
+    /* .service ul,
+    .top>div>div ul {
+        list-style: none;
+        padding-left: 0;
+        margin-top: 5px;
+    } */
+
+    /* .top>div {
+        text-align: left;
     } */
 </style>
 
 <footer>
 
-    <p>Enjoy the bananas</p>
-    <p>BANANAASSS</p>
-    <div class="add"> <!--address -->
-        <p>Rosario Cavite, PH</p>
-        <p>Tel: 09*******34</p>
+    <!-- <p>Enjoy the bananas</p>
+    <p>BANANAASSS</p> -->
+    <!-- bbtoom -->
+    <div class="top">
+        <!--address -->
+        <div class="add">
+            <li>Rosario Cavite, PH</li>
+            <li>Tel: 09*******34</li>
+        </div>
+
+        <!-- services -->
+        <div class="service">
+            <div class="topper">
+                <h6>Terms</h6>
+                <ul>
+                    <li>Terms of services</li>
+                    <li>Privacy Policy</li>
+                    <li>Cookie Policy</li>
+
+                </ul>
+            </div>
+        </div>
+
+        <!-- resources -->
+        <div>
+            <div>
+                <h6>Resources</h6>
+                <ul>
+                    <li>Moodboard</li>
+                    <li>Roadmap</li>
+                </ul>
+            </div>
+        </div>
+
     </div>
-    <div class="footi">
-        <div class="fortie">
-            <a id="cop">Copyrite @ 2025. - All Rights Reserved</a>
+
+
+    <!-- botom -->
+    <div class="bot">
+        <!-- copyright -->
+        <div>
+            <div class="footi">
+                <div class="fortie">
+                    <p id="cop">Copyright @ 2025. - All Rights Reserved</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- footer board -->
+    <!-- <div class=fooboard>
+        Moodboard
+    <div class="mood">
+        <div class="board">
+            <a href="/moodboard">Moodboard</a>
+        </div>
+    </div> -->
+
+    <!-- Roadmap -->
+    <!-- <div>
+            <div>
+                <a href="roadmap">Roadmap</a>
+            </div>
+        </div>
+    </div> -->
+
+    <!-- retunr to to  -->
+    <div>
+        <div class="return">
+            <a href="#top">Return to top</a>
         </div>
     </div>
 </footer>
